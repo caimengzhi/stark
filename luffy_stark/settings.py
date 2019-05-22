a8f5fc9934c4cdad07513c8b2c5975f4da7317a5
caimengzhi <caimengzhi@loocha.com>
2019-05-22 11:32:39 +0800

    start 准备环境

--- luffy_stark/settings.py
+++ luffy_stark/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app01.apps.App01Config',
+    'app02.apps.App02Config',
+    'stark.apps.StarkConfig',
 ]
 
 MIDDLEWARE = [
